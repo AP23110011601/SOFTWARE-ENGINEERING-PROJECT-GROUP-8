0d36cb2866969e4c652577d3ba1b093e6cd68c8a
kusumanjali-somisetty <kusu5517@gmail.com>
2026-04-20 11:22:49 +0530

    Update API URL to use environment variable

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -49,7 +49,7 @@ const Dashboard = () => {
   const fetchDashboardData = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await fetch("http://localhost:5000/api/sensor", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/apisensor`,{
         headers: {
           Authorization: `Bearer ${token}`,
         },
